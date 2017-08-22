063f9f8966f4642cea03aa6e551aa818dc37bca0
Atul Gupta <atul.iseet@yahoo.co.in>
2017-08-23 01:43:50 +0530

    Async Issue

--- index.php
+++ index.php
@@ -119,8 +119,57 @@
 			//google.charts.setOnLoadCallback(drawChart);
 		}
 
+		function zeros(dimensions) {
+			var array = [];
+			for (var i = 0; i < dimensions[0]; ++i) {
+				array.push(dimensions.length == 1 ? 0 : zeros(dimensions.slice(1)));
+			}
+			return array;
+		}
+
+
+		function getQuestions(contest) {
+			alert("Getting Qns for - "+contest);
+			var ar=["PALINGAM","GCAC","CHEFMOVR","RAINBOWA","CHEFFA","STRINGRA","HILLJUMP","WALKBT","MATDW"];
+			return ar;
+		}
+
+
 		function all(contest) {
 			//alert(contest + " all contest Data");
+			var ar=getQuestions(contest);
+			var numberOfQuestion = ar.length;
+			var dataAr=zeros([numberOfQuestion,8]);
+			var i=0,j=0;
+			dataAr[i][j]=10;
+			console.log(dataAr[0][0]);
+			$("#content").empty();
+			for(var i=0;i<numberOfQuestion;i++) {
+				var question=ar[i];
+				var path=contest+'/'+question+'/'+'starData.json';
+				var json=$.getJSON(path)
+				.done(function( json ) {
+					var end=Object.keys(json["time"]);
+					//$("#content").append( "JSON Data: " + json["time"][end[end.length-1]][7] +" ");
+					var len=8; // Because there fixed 8 starts : json["time"][end[end.length-1]].length;
+					for(var j=0;j<len;j++) {
+						
+					dataAr[i][j]=json["time"][end[end.length-1]][j];
+						//dataAr[i][j]+=json["time"][end[end.length-1]][j];
+					}
+					$("#content").append("<br>");
+					//$("#content").append( "JSON Data: " + JSON.stringify(Object.keys(json["time"]["1503233085"]), null, 2) +"<br><br>");
+					//console.log( "JSON Data: " + JSON.stringify(json, null, 2) );
+					
+				})
+				.fail(function( jqxhr, textStatus, error ) {
+					var err = textStatus + ", " + error;
+					console.log( "Request Failed: " + err );
+					return null;
+				});
+
+			}
+			console.log(dataAr);
 			return;
 		}
 		function oneQuestion(contest,question) {
