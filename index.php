8d6093c3727d3366253d273830477fa4d96fd6ae
Atul Gupta <atul.iseet@yahoo.co.in>
2017-08-23 16:47:14 +0530

    Graph resolves - for All and single

--- index.php
+++ index.php
@@ -36,6 +36,7 @@
 			<label>Questions </label>
 			<select class="browser-default" id="question" onchange="render()">
 				<option value="" disabled selected>Choose Question</option>
+				<option value="" class="red-text">clear</option>				
 				<option value="CHEFFA">CHEFFA</option>
 				<option value="CHEFMOVR">CHEFMOVR</option>
 				<option value="GCAC">GCAC</option>
@@ -49,6 +50,7 @@
 			<label>Types </label>
 			<select class="browser-default" id="type" onchange="render()">
 				<option value="" disabled selected>Choose Type</option>
+				<option value="" class="red-text">clear</option>
 				<option value="star">Star</option>
 				<option value="lang">Language</option>
 			</select>
@@ -64,10 +66,6 @@
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend enim non metus malesuada, a ullamcorper velit luctus. Vestibulum quis blandit ex. Vivamus ut malesuada risus, vel hendrerit mi. Nunc elit nibh, dictum sit amet nisi ac, sollicitudin faucibus felis. Curabitur mattis dui et molestie imperdiet. Morbi ultricies diam at malesuada accumsan. Praesent in risus a tellus feugiat sodales vitae et nisi.</p>
 
 					<p>Fusce non finibus erat, vitae hendrerit est. Ut vestibulum blandit libero. Curabitur at dignissim turpis. Donec sed orci hendrerit, convallis diam euismod, vestibulum arcu. Maecenas eleifend commodo felis, et volutpat metus dignissim vitae. Vestibulum non lectus non sapien faucibus finibus. Morbi dictum, nisl a blandit blandit, diam nibh dignissim urna, sit amet placerat quam velit egestas erat.</p>
-
-					<p>Morbi ullamcorper quam non sem pretium interdum. Maecenas varius feugiat enim, quis ullamcorper eros imperdiet id. Sed faucibus arcu quis nibh imperdiet cursus. Curabitur convallis erat non arcu consectetur dignissim. Praesent varius felis vel lectus pretium eleifend. Sed risus magna, consectetur non urna a, lobortis consequat eros. Ut nec libero finibus, fringilla dui id, mollis felis. In hac habitasse platea dictumst. Suspendisse aliquam accumsan metus at auctor. Aenean at nibh quis leo aliquet aliquam. Nam massa nisl, mattis vel dolor at, placerat ornare nulla. Donec bibendum dolor magna, quis volutpat dolor molestie vitae. Vivamus diam ligula, eleifend eget ornare commodo, ultricies eget orci. Donec lacinia tempor ex eu semper. Curabitur auctor eros in felis semper dapibus. Aenean semper placerat ligula.</p>
-
-					<p>Nulla facilisi. Nunc vel imperdiet dui. Pellentesque iaculis eros at nulla auctor, ac maximus ante lacinia. Aliquam faucibus dolor ut neque iaculis, in scelerisque orci ornare. Sed volutpat at arcu id malesuada. Nunc in nibh sit amet metus tincidunt auctor vel id quam. Duis semper rhoncus enim eu rutrum. Mauris mollis volutpat arcu at efficitur. Sed massa purus, tempus ut pulvinar faucibus, luctus in tortor. Curabitur vitae pretium mauris, in posuere ligula. Vivamus pretium, velit id semper finibus, mi ex elementum justo, non ornare felis lacus at sapien.</p>
 
 					<p>Maecenas viverra mauris sit amet orci vehicula, sed placerat libero dictum. Suspendisse egestas orci nisl, vel tincidunt est sollicitudin quis. Sed commodo tempus lacinia. Etiam eu maximus ligula. Ut sit amet ornare purus, in placerat magna. Fusce et faucibus sem. Nullam at purus cursus ex interdum fermentum. Maecenas posuere nulla et consectetur rhoncus. Phasellus eu ante vel arcu facilisis vestibulum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut mattis finibus lorem congue auctor. Quisque suscipit sed sapien ac molestie.</p>
 				</div>
@@ -90,9 +88,11 @@
 		var contest = $("#contest").val();
 		var question = $("#question").val();
 		var type = $("#type").val();
+		var varvtitle;
 		var graphArray;
 
 		function render() {
+			//$("#content").empty();
 			contest = $("#contest").val();
 			question = $("#question").val();
 			type = $("#type").val();
@@ -106,17 +106,25 @@
 			]
 			if(contest && !question && !type) {
 				all(contest);
+				vartitle=contest;
 			}
 			else if(contest && question && !type) {
-				oneQuestion(contest,question);
+				//oneQuestion(contest,question);
 			}
 			else if(contest && question && type){
 				single(contest,question,type);
+				vartitle=contest+' > '+question+' > ';
+				if(type=='star')
+					vartitle = vartitle+'Star';
+				else
+					vartitle = vartitle+'Language';
+				vartitle = vartitle+' Based';
 			}
 			else {
 				//alert("Invalid Selection");
 			}
-			//google.charts.setOnLoadCallback(drawChart);
+			google.charts.load('current', {'packages':['corechart']});
+			google.charts.setOnLoadCallback(drawChart);
 		}
 
 		function zeros(dimensions) {
@@ -129,7 +137,7 @@
 
 
 		function getQuestions(contest) {
-			alert("Getting Qns for - "+contest);
+			//alert("Getting Qns for - "+contest);
 			var ar=["PALINGAM","GCAC","CHEFMOVR","RAINBOWA","CHEFFA","STRINGRA","HILLJUMP","WALKBT","MATDW"];
 			return ar;
 		}
@@ -140,9 +148,11 @@
 			var ar=getQuestions(contest);
 			var numberOfQuestion = ar.length;
 			var dataAr={};
-			$("#content").empty();
+			var tempAr=[];
+			tempAr.push('Time');
 			for(var i=0;i<numberOfQuestion;i++) {
 				var question=ar[i];
+				tempAr.push(question);
 				var path=contest+'/'+question+'/'+'starData.json';
 				$.ajax({
 					url: path,
@@ -163,7 +173,23 @@
 					}
 				});
 			}
-			console.log(dataAr);
+			//console.log(dataAr);
+			graphArray=[];
+			graphArray.push(tempAr);
+			console.log(graphArray);
+			for(stamps in dataAr) {
+				tempAr=[];
+				var epoch = new Date( stamps*1000 );
+				tempAr.push(stamps);
+				for(var i=0;i<numberOfQuestion;i++) {
+					var question=ar[i];
+					if(!dataAr[stamps].hasOwnProperty(question))
+						tempAr.push(0);
+					else
+						tempAr.push(dataAr[stamps][question]);
+				}
+				graphArray.push(tempAr);
+			}
 			return;
 		}
 		function oneQuestion(contest,question) {
@@ -172,24 +198,78 @@
 		}
 		function single(contest,question,type) {
 			var path=contest+'/'+question+'/'+type+'Data.json';
-			$.getJSON(path)
-			.done(function( json ) {
-				console.log( "JSON Data: " + JSON.stringify(json, null, 2) );
-			})
-			.fail(function( jqxhr, textStatus, error ) {
-				var err = textStatus + ", " + error;
-				console.log( "Request Failed: " + err );
+			//alert(contest + " all contest Data");
+			var xArr={};
+			var dataAr={};
+			var tempAr=[];
+			tempAr.push('Time');
+			//tempAr.push(question);
+			$.ajax({
+				url: path,
+				dataType: 'json',
+				async: false,
+				success: function(json) {
+					for (stamps in json["time"]) {
+						var len=json["time"][stamps].length;
+						var sum=0;
+						if(!dataAr.hasOwnProperty(stamps))
+							dataAr[stamps]={};
+						if(type=='star') {
+							for(var j=0;j<len;j++) {
+								if(!xArr.hasOwnProperty(j))
+									xArr[j]=j;
+								sum=sum+(json["time"][stamps][j]);
+								dataAr[stamps][j]=json["time"][stamps][j];
+							}
+						}
+						else if(type=='lang') {
+							for(uages in json["time"][stamps]) {
+								if(!xArr.hasOwnProperty(uages))
+									xArr[uages]=uages;
+								sum=sum+(json["time"][stamps][uages]);
+								dataAr[stamps][uages]=json["time"][stamps][uages];
+							}
+						}
+						dataAr[stamps][question]=sum;
+
+					}
+				}
 			});
-			//alert(contest + question + type + "Question + Contest + Type");
+			console.log(xArr);
+			console.log(dataAr);
+			graphArray=[];
+			for (xVal in xArr) {
+				tempAr.push(xVal+'★');
+			}
+			graphArray.push(tempAr);
+			console.log(graphArray);
+			for(stamps in dataAr) {
+				tempAr=[];
+				tempAr.push(stamps);
+				//if(!dataAr[stamps].hasOwnProperty(question))
+				//	tempAr.push(0);
+				//else
+				//	tempAr.push(dataAr[stamps][question]);
+				var epoch = new Date( stamps*1000 );
+				for (xVal in xArr) {
+					if(!dataAr[stamps].hasOwnProperty(xVal))
+						tempAr.push(0);
+					else
+						tempAr.push(dataAr[stamps][xVal]);
+				}
+				graphArray.push(tempAr);
+			}
 			return;
 		}
 		function drawChart() {
 			var data = google.visualization.arrayToDataTable(graphArray);
-
+			//var wd=$('#content').width();
+			//var ht=$('.row').height();
 			var options = {
-				title: 'Company Performance',
+				title: vartitle,
 				curveType: 'function',
-				legend: { position: 'right' }
+				//height: ht,
+				//width: wd
 			};
 
 			var chart = new google.visualization.LineChart(document.getElementById('content'));
