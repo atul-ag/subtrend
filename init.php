1c3187c239158e8e08685ee8c4db2adcc891c283
EC2 Default User <ec2-user@ip-172-31-23-244.us-west-2.compute.internal>
2017-08-25 06:47:09 +0000

    For LOCAUG17

--- init.php
+++ init.php
@@ -1,8 +1,11 @@
-<?php
-echo time()."<br>";
+ <?php
+echo time()." - Initialized<br>";
+$contestName = "LOCAUG17";
+$contestQuestions = array("ALATE","ALTSUB","GTREE","KMAX","PRINCESS");
+/*
 $contestName = "AUG17";
-$contestQuestions = array("PALINGAM","GCAC","CHEFMOVR","RAINBOWA","CHEFFA","STRINGRA","HILLJUMP","WALKBT","MATDW");
-//$contestQuestions = array("CHEFFA","STRINGRA","HILLJUMP","WALKBT","MATDW");
+$contestQuestions = array("PALINGAM","GCAC","CHEFMOVR","RAINBOWA","CHEFFA","    STRINGRA","HILLJUMP","WALKBT","MATDW");
+//$contestQuestions = array("CHEFFA","STRINGRA","HILLJUMP","WALKBT","MATDW")    ;
 //var_dump($contestQuestions);
-
+*/
 ?>
